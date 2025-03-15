eebbdc2a64b9c3571c2838154b87ca69eb175b9b
Sughra Mumshad <sughramumshad@gmail.com>
2025-03-15 20:51:34 +0500

    Add files via upload

---
+++ Lab#1_Sughra_Mumshad.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+//                      ********************TASK1******************
+	
+//	cout<<"Item\tPrice\tQuality\n";
+//	cout<<"Pen\t$1\t10\n";
+//	cout<<"Pencil\t$2\t20\n";
+//	cout<<"Eraser\t$3\t30\n";
+	
+//	                    *******************TASK2****************
+
+//  cout<<"She said,\ Let's learn C++!\"\n";
+//  cout<<"To start save your file in C:\\C++_Projects\\Lab1\n";
+//  cout<<"Good Luck!\n";
+	
+//	                  ************************ TASK3**********************
+
+//  cout<<"***\n        \n        \n        \n        \n***";
+	
+//	OR
+
+//	cout<<"****\n";
+//	cout<<"*\t *\n";
+//	cout<<"*\t *\n";
+//	cout<<"*\t *\n";
+//	cout<<"****\n";
+
+
+//                    *********************TASK4***********************
+
+//   cout<<"-----------\n|         |\n|  45+78  |\n|  = 123  |\n|         |\n-----------";	
+
+
+
+//                      ***********************TASK5*******************
+
+//    cout<<"\tReturant XYZ\n";
+//	cout<<"Item\tquantity    price\n";
+//	cout<<"Pizza\t2          $15\n";
+//	cout<<"Pasta\t1          $12\n";
+//	cout<<"Soda\t3          $3\n";
+//	cout<<"Total              $51\n";
+  
+//                     ******************TASK6*********************
+
+//    cout<<"\t\"The Road Not Taken\"\n";
+//	cout<<"\t  by Robert Frost\n";
+//	cout<<"Two roads diverged in a yellow wood,\n";
+//	cout<<"And sorry i could not travel both\n";
+//	cout<<"And be one taveler, long i started\n";
+//	cout<<"To where it bent in the undergrowth\n";
+
+//                     ***************** TASK7*****************
+
+//	cout<<"Person A: \"Hello! How are you?\n";
+//	cout<<"Person B: \"I am fine, Thank you! How about you?\n";
+//	cout<<"Person B: \"I am doing great. Thanks for asking!\n";
+
+
+
+	return 0;
+}
